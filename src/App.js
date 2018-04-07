b7517230559fc419a3ae9c1863bb6affa672c09c
Arm Supakorn <rainstorm.armiie@gmail.com>
2018-04-07 15:37:23 +0700

    add html

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Arm</p>
+        <p>Slack Test</p>
       </div>
     );
   }
